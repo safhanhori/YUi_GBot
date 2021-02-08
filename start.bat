9923315654a93b26533580e95aaa010857c3b0f4
NetSHELL-Team <76968186+NetSHELL-Team@users.noreply.github.com>
2021-02-08 22:26:15 +0530

    lol update kiya

--- start.bat
+++ start.bat
@@ -1,10 +1,10 @@
 @echo off
-TITLE phantom Robot
+TITLE Yui_Gbot Robot
 rem This next line removes any fban csv files if they exist in root when bot restarts. 
 del *.csv
-py -3.7 --version
+py -3.8 --version
 IF "%ERRORLEVEL%" == "0" (
-    py -3.7 -m Yui_Gbot
+    py -3.8 -m Yui_Gbot
 ) ELSE (
     py -m Yui_Gbot
 )
