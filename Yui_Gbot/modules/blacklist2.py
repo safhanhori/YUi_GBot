9f34e7440be619272bf3024dd7d7d64965e33a23
NetSHELL-Team <76968186+NetSHELL-Team@users.noreply.github.com>
2021-02-28 11:43:25 +0530

    Update blacklist2.py

--- Yui_Gbot/modules/blacklist2.py
+++ Yui_Gbot/modules/blacklist2.py
@@ -352,7 +352,7 @@ def __chat_settings__(chat_id, user_id):
 	return "There are `{} `blacklisted stickers.".format(blacklisted)
 
 def __stats__():
-	return "<b>» {} BlackList Stickers, Across </b>{}<B> Chats.</B>\n".format(sql.num_stickers_filters(), sql.num_stickers_filter_chats())
+	return "<b>» {} BlackList Stickers, Across </b>{}<b> Chats.</b>\n".format(sql.num_stickers_filters(), sql.num_stickers_filter_chats())
 
 __help__ = """
 Blacklist sticker is used to stop certain stickers. Whenever a sticker is sent, the message will be deleted immediately.
