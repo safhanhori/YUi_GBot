7a7f1c1da6baac43d021681440a53049d37b39cd
NetSHELL-Team <76968186+NetSHELL-Team@users.noreply.github.com>
2021-02-28 10:04:36 +0530

    HO GYA #BC

--- Yui_Gbot/modules/warns.py
+++ Yui_Gbot/modules/warns.py
@@ -366,8 +366,8 @@ def set_warn_strength(bot: Bot, update: Update, args: List[str]):
 
 
 def __stats__():
-    return (f"{sql.num_warns()} Warns, Across {sql.num_warn_chats()} Chats.\n\n"
-            f"{sql.num_warn_filters()} Warn Filters, Across {sql.num_warn_filter_chats()} Chats.")
+    return (f"{sql.num_warns()}<b>» Warns, Across</b>{sql.num_warn_chats()}<b>Chats</b>\n\n"
+            f"{sql.num_warn_filters()}<b>» Warn Filters, Across {sql.num_warn_filter_chats()}<b>Chats</b>")
 
 
 def __import_data__(chat_id, data):
